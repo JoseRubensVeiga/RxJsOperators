12ffcb82a4b00b80384bbae542fd15e961167b1a
José <jose.rubens.veiga@gmail.com>
2021-05-26 19:26:12 -0300

    bindCallback

--- src/app/pages/bind-callback/bind-callback.component.ts
+++ src/app/pages/bind-callback/bind-callback.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { bindCallback, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bind-callback',
   templateUrl: './bind-callback.component.html',
-  styleUrls: ['./bind-callback.component.scss']
+  styleUrls: ['./bind-callback.component.scss'],
 })
 export class BindCallbackComponent implements OnInit {
+  subscription?: Subscription;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    const fn = bindCallback(this.fakeFnWithCallback);
+
+    fn('bindCallback :)').subscribe((value) => {
+      console.log(value);
+    });
   }
 
+  private fakeFnWithCallback<T>(value: T, cb: (param: T) => any): any {
+    return cb(value);
+  }
 }
